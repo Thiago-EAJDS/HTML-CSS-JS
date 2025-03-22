5030990b72b75c85a7d7722ce343f20dd959748f
Thiago Emanuel <thiagoeajds@gmail.com>
2025-03-21 23:12:21 -0300

    v4

--- contagem vogais/script.js
+++ contagem vogais/script.js
@@ -7,8 +7,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const contarO = document.getElementById("contarO");
     const contarU = document.getElementById("contarU");
 
-    // duvido que esteja vendo os codigos tudo, isso aq é máfia
-
     inputText.addEventListener("input", function () {
         const text = inputText.value.toLowerCase();
         const vogais = { 'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0 };
